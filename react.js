7b52f591ef7a999d9b7287e1ffcae542691f8395
ying_coding0722 <42222462+leiqianwang@users.noreply.github.com>
2023-09-19 16:17:43 -0400

    Update react.js

--- react.js
+++ react.js
@@ -33,3 +33,15 @@ function App() {
 }
 
 export default App;
+
+
+
+//In this React code:
+
+//We import React and necessary hooks (useState, useEffect).
+//We create a functional component called App.
+//We use the useState hook to manage the state of the quote data.
+// We use the useEffect hook to fetch a random quote when the component mounts.
+// The fetchQuote function fetches a random quote from the API and updates the state with the received data.
+// Inside the return statement, we use JSX to display the quote and a button to fetch a new quote when clicked.
+// Don't forget to replace the CSS styles in the App.css file with the styles from your original CSS file. Also, make sure to import this App component in your main React application file and render it.
